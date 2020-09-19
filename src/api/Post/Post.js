86c8bb678bc2894c286a34071344c1cb11064546
seungju <kingsky32@gmail.com>
2020-09-20 00:24:18 +0900

    Limit Comment

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Post: {
     files: ({ id }) => prisma.post({ id }).files(),
-    comments: ({ id }) => prisma.post({ id }).comments(),
+    comments: ({ id }) => prisma.post({ id }).comments({ first: 1 }),
     user: ({ id }) => prisma.post({ id }).user(),
     likes: ({ id }) => prisma.post({ id }).likes(),
     isLiked: (parent, _, { request }) => {
